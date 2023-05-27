f32951dcf2765c2899c29ee3747d3cdbad6d887b
danessti <danessti@gmail.com>
2023-05-27 01:45:39 -0500

    Ejercicio 3 completo

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
+
+    /*Hacer un programa que permita ingresar 10 valores a un array seguido muestre cuantos son positivos, cuantos
+    * negativos y cuantos son nulos*/
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Inrgesa el tamano del arreglo");
+        int tamano = sc.nextInt();
+
+        int[] vector = new int[tamano];
+
+        for (int i = 0; i < tamano; i++) {
+
+            System.out.println("Ingresa el valor "+(i+1));
+            vector[i] = sc.nextInt();
+
+        }
+
+        int positivos = 0;
+        int nulos = 0;
+        int negativos = 0;
+
+        for (int i = 0; i < tamano; i++) {
+
+            if (vector[i] > 0) {
+                positivos++;
+            } else if (vector[i] == 0) {
+                nulos++;
+            } else {
+                negativos++;
+            }
+
+        }
+
+        System.out.println("Hay "+positivos+" positivos \n");
+        System.out.println("Hay "+negativos+" negativos \n");
+        System.out.println("Hay "+nulos+" nulos ");
 
     }
 
