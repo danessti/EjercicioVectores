42d484921ef8b5bd70e795c4f25373fa6e3807b5
danessti <danessti@gmail.com>
2023-05-27 15:48:57 -0500

    Actualizado

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -8,9 +8,9 @@ public class Ejercicio2 {
 
         Scanner sc = new Scanner(System.in);
 
-        int[] vector = new int[5];
+        int[] vector = new int[10];
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
 
             System.out.println("Vector "+(i+1));
             vector[i] = sc.nextInt();
@@ -20,7 +20,7 @@ public class Ejercicio2 {
         int pares = 0;
         int impares = 0;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
 
             if (vector[i] % 2 ==0){
                 pares++;
