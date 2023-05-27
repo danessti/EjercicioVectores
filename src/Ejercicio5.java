42ee7873ab188798fadfe0de753752c8bb471f87
danessti <danessti@gmail.com>
2023-05-27 17:52:13 -0500

    Nuevo ejercicio terminado

---
+++ src/Ejercicio5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+    /*Haz un vector numerico de 10 posiciones dados por el usuario y muestre los elementos que son mayores al numero
+    * dado por el usuario*/
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] vector = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Ingrese el valor "+(i+1));
+            vector[i] = sc.nextInt();
+
+        }
+
+        System.out.println("");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(vector[i]);
+        }
+
+    }
+
+}
